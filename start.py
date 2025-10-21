e2f3cd33607a9a7b9d1e900120f22bf6eda12826
expiol <black.by.hong@gmail.com>
2025-10-21 16:46:13 +0800

    update

--- start.py
+++ start.py
@@ -7,7 +7,6 @@ import os
 from src.crwal.scraper import run
 
 if __name__ == "__main__":
-    # === 参数区 ===
     COUNT = 200  # 抓取数量
     OUT_PATH = os.path.join("data", "YangHong+20251234.csv")  
     LOG_PATH = os.path.join("logs", "scrape.log")  
